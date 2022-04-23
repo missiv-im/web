e8cdce9ba0d05c2fe156a76965fe65cb46dad566
Arblade <raphael.arblade@gmail.com>
2022-04-23 02:14:15 +0200

    version start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import { getPredefinedBootstrapNodes, Waku } from "js-waku";
 import * as React from "react";
+import InputMessageBar from "./InputMessageBar"
 
+import "./App.css"
+import { Alert } from "shards-react";
 // const topic = "/murmur/1/global/proto";
+
+function Header(){
+	return <header></header>
+}
+
+function Log(props:any){
+	
+	const [leftTime, setLeftTime] = React.useState(0);
+	const [visible, setVisible] = React.useState(true);
+	React.useEffect(() => {
+		console.log(props.message);
+		const interval = setInterval(() => {
+		  setLeftTime(leftTime=> leftTime + 1);
+		  if (leftTime >5){
+			clearInterval(interval);
+			setVisible(false)	;
+		  }
+		}, 1000);
+	});
+		return <Alert fade={true} className="log-alert" open={visible}>{props.message+""}</Alert>
+	
+}
 
 function App() {
 	const [waku, setWaku] = React.useState<Waku | undefined>();
@@ -24,10 +49,20 @@ function App() {
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>Waku node's status: {wakuStatus}</p>
-				<p>Nb peers: {waku ? waku!.relay.getPeers().size : ""}</p>
-			</header>
+			<Header />
+			<div className="App-content">
+			
+			<div className="App-header">
+				
+				<Log message={"Waku node's status: "+ wakuStatus}/>
+				<Log message={"Nb peers:" + (waku ? waku!.relay.getPeers().size : "") + ""}/>
+				
+				
+				
+			</div>
+			</div>
+			
+			<InputMessageBar />
 		</div>
 	);
 }
